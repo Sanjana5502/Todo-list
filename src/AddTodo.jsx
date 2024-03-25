97d35eebbfcab7cd8e0e89b26ab59c1ded665c5b
Sanjana5502 <sanjanachhipa55@gmail.com>
2024-03-25 22:24:00 +0530

    first commit

--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,35 +1,3 @@
-// import React, { Component } from 'react'
-
-// class AddTodo extends Component {
-//     state ={
-//         context: ''
-//     }
-//     handleChange =(e)=>{
-//         this.setState({
-//             context: e.target.value
-//         })
-//     }
-//     handleSubmit =(e)=>{
-//         e.preventDefault();
-//         this.props.addTodo(this.state);
-//         this.setState({
-//           context : ''
-//         })
-//     }
-//   render(){
-//     return (
-//     <div>
-//       <form onSubmit={this.handleSubmit}>
-//         <label> Add new todo:</label>
-//         <input type='text' onChange={this.handleChange} value={this.state.context}/>
-//       </form>
-//     </div>
-//   )
-// }
-// }
-
-// export default AddTodo
-// AddTodo.js
 import React, { Component } from 'react';
 
 class AddTodo extends Component {
@@ -43,10 +11,14 @@ class AddTodo extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    if (!this.state.context.trim()) {
+      alert('Please enter a todo.');
+      return;
+    }
     this.props.addTodo(this.state);
     this.setState({ context: '' });
   }
-
+  
   render() {
     return (
       <div>
